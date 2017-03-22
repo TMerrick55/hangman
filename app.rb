407a5ac7b3beb853090d985b6313c83b15658c6e
Shirley Scott <shirleyscott@Shirleys-iMac-2.local>
2017-03-21 20:10:20 -0400

    needed to add layouts to get it to show

--- app.rb
+++ app.rb
@@ -3,19 +3,34 @@ require 'rubygems'
 
 class HangManApp <Sinatra::Base
 
+	# get '/' do
+	# 	'Hello World!'
+	# end
+
+
 	get '/' do
-		"Hello World"
+		erb :players
+
 	end
 
-	# get '/' do
-	# 	erb :players
+	post '/player' do
+		backend_player1 = params[:p1_name_input]
+		backend_player2 = params[:p2_name_input]
 
-	# end
+		# "Player 1 name is #{backend_player1} and player 2 name is #{backend_player2}"
+		erb :secret_word, :locals => {player1: backend_player1, player2: backend_player2}
+	end
 
-	# post '/player' do
-	# 	player = params[:player]
+	post '/secret_word' do
+		backend_player1 = params[:p1_name_input]
+		backend_player2 = params[:p2_name_input]
+		backend_secret = params[:secret_word]
 
-
-	# end
+		# "player 1 is #{backend_player1} and player 2 name is #{backend_player2} and the word is #{backend_secret}"
+		erb :next_file, 
+	end
 
 end
+
+
+
---
+++ config.ru
@@ -0,0 +1,4 @@
+require './app.rb'
+HangManApp.run!
+
+#rackup
